180286ad7e1767f63954b70158742b301dc66810
David Gvili <dudugvili@yahoo.com>
2024-10-14 21:03:43 +0000

    work

--- results/json_to_excel.py
+++ results/json_to_excel.py
@@ -1,5 +1,5 @@
 import pandas as pd
-json_path = "EngFinalProject/results/results.json"
+json_path = "results/results.json"
 df = pd.read_json(json_path)
 print(df.head())
-df.to_excel("results_gpu.xlsx")
+df.to_excel("results_dudu_141024.xlsx")
